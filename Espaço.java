91045e18b6ff42dc4fdc1045bdac71f24018929a
Rodrigo84885 <52356419+Rodrigo84885@users.noreply.github.com>
2020-03-03 16:20:31 +0000

    Add files via upload

---
+++ Espaço.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Pedro Fernandes
+ */
+public class Espaço {
+    private int id_space;
+    private String name_space;
+
+    public int getId_space() {
+        return id_space;
+    }
+
+    public void setId_space(int id_space) {
+        this.id_space = id_space;
+    }
+
+    public String getName_space() {
+        return name_space;
+    }
+
+    public void setName_space(String name_space) {
+        this.name_space = name_space;
+    }
+    
+    
+}
